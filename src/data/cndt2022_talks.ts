a63f5c0ac8bf28800eb00a4eb70a61c61e879aa1
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-02-03 20:53:14 +0900

    add talks

---
+++ src/data/cndt2022_talks.ts
@@ -0,0 +1,4 @@
+import type { Talk } from "../types/talk";
+
+export const CNDT2022Talks: Talk[] = [
+];
---
+++ src/data/cnsec2022_talks.ts
@@ -0,0 +1,5 @@
+import type { Talk } from "../types/talk";
+
+
+export const CNSEC2022Talks: Talk[] = [
+];
