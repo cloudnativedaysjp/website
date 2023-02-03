ba8a10b644dd4485dbfbbf5785e190df9ebe5200
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-02-03 20:53:14 +0900

    add talks

---
+++ src/data/talks.ts
@@ -0,0 +1,8 @@
+import type { Talk } from "../types/talk";
+import { CNDT2022Talks } from "./cndt2022_talks";
+import { CNSEC2022Talks } from "./cnsec2022_talks";
+
+export const talkMap = new Map<string, Talk[]>([
+    ["cndt2022", CNDT2022Talks],
+    ["cnsec2022", CNSEC2022Talks]
+]) 
