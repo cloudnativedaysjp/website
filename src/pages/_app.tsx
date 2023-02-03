18e42ab09fe0c310274021bee09d17ed6d9a959e
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-02-03 18:58:10 +0900

    init astro and display conferences

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-import '../styles/global.css'
-
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
