25ec8cab139ece1b10f7e5c446c0b555bd4ad856
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-02-12 18:25:08 +0900

    Revert "Add speakers of CNDT2022"

--- src/data/speakers.ts
+++
@@ -1,6 +0,0 @@
-import type { Speaker } from "../types/speaker";
-import { CNDT2022Speakers } from "./cndt2022_speakers";
-
-export const speakerMap = new Map<string, Speaker[]>([
-    ["cndt2022", CNDT2022Speakers],
-]) 
