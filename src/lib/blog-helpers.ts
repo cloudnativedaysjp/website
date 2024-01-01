51eb0e8d5b5e46a8a478e73653eedd9697d79218
Kazuto Kusama <jacopen@gmail.com>
2024-01-02 01:02:19 +0900

    Update latest astro-notion-blog

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -17,7 +17,7 @@ export const filePath = (url: URL): string => {
 
 export const extractTargetBlocks = (
   blockType: string,
-  blocks: Block[],
+  blocks: Block[]
 ): Block[] => {
   return blocks
     .reduce((acc: Block[], block) => {
@@ -27,39 +27,39 @@ export const extractTargetBlocks = (
 
       if (block.ColumnList && block.ColumnList.Columns) {
         acc = acc.concat(
-          _extractTargetBlockFromColums(blockType, block.ColumnList.Columns),
+          _extractTargetBlockFromColums(blockType, block.ColumnList.Columns)
         )
       } else if (block.BulletedListItem && block.BulletedListItem.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.BulletedListItem.Children),
+          extractTargetBlocks(blockType, block.BulletedListItem.Children)
         )
       } else if (block.NumberedListItem && block.NumberedListItem.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.NumberedListItem.Children),
+          extractTargetBlocks(blockType, block.NumberedListItem.Children)
         )
       } else if (block.ToDo && block.ToDo.Children) {
         acc = acc.concat(extractTargetBlocks(blockType, block.ToDo.Children))
       } else if (block.SyncedBlock && block.SyncedBlock.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.SyncedBlock.Children),
+          extractTargetBlocks(blockType, block.SyncedBlock.Children)
         )
       } else if (block.Toggle && block.Toggle.Children) {
         acc = acc.concat(extractTargetBlocks(blockType, block.Toggle.Children))
       } else if (block.Paragraph && block.Paragraph.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.Paragraph.Children),
+          extractTargetBlocks(blockType, block.Paragraph.Children)
         )
       } else if (block.Heading1 && block.Heading1.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.Heading1.Children),
+          extractTargetBlocks(blockType, block.Heading1.Children)
         )
       } else if (block.Heading2 && block.Heading2.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.Heading2.Children),
+          extractTargetBlocks(blockType, block.Heading2.Children)
         )
       } else if (block.Heading3 && block.Heading3.Children) {
         acc = acc.concat(
-          extractTargetBlocks(blockType, block.Heading3.Children),
+          extractTargetBlocks(blockType, block.Heading3.Children)
         )
       } else if (block.Quote && block.Quote.Children) {
         acc = acc.concat(extractTargetBlocks(blockType, block.Quote.Children))
@@ -74,7 +74,7 @@ export const extractTargetBlocks = (
 
 const _extractTargetBlockFromColums = (
   blockType: string,
-  columns: Column[],
+  columns: Column[]
 ): Block[] => {
   return columns
     .reduce((acc: Block[], column) => {
@@ -87,7 +87,7 @@ const _extractTargetBlockFromColums = (
 }
 
 export const buildURLToHTMLMap = async (
-  urls: URL[],
+  urls: URL[]
 ): Promise<{ [key: string]: string }> => {
   const htmls: string[] = await Promise.all(
     urls.map(async (url: URL) => {
@@ -107,7 +107,7 @@ export const buildURLToHTMLMap = async (
         .finally(() => {
           clearTimeout(timeout)
         })
-    }),
+    })
   )
 
   return urls.reduce((acc: { [key: string]: string }, url, i) => {
@@ -140,12 +140,12 @@ export const getTagLink = (tag: string) => {
 
 export const getPageLink = (page: number, tag: string) => {
   if (page === 1) {
-    return tag ? getTagLink(tag) : pathJoin(BASE_PATH, '/posts')
+    return tag ? getTagLink(tag) : pathJoin(BASE_PATH, '/')
   }
   return tag
     ? pathJoin(
         BASE_PATH,
-        `/posts/tag/${encodeURIComponent(tag)}/page/${page.toString()}`,
+        `/posts/tag/${encodeURIComponent(tag)}/page/${page.toString()}`
       )
     : pathJoin(BASE_PATH, `/posts/page/${page.toString()}`)
 }
@@ -180,7 +180,74 @@ export const buildHeadingId = (heading: Heading1 | Heading2 | Heading3) => {
 }
 
 export const isTweetURL = (url: URL): boolean => {
-  return /https:\/\/twitter.com\/[^/]+\/status\/[\d]+/.test(url.toString())
+  if (
+    url.hostname !== 'twitter.com' &&
+    url.hostname !== 'www.twitter.com' &&
+    url.hostname !== 'x.com' &&
+    url.hostname !== 'www.x.com'
+  ) {
+    return false
+  }
+  return /\/[^/]+\/status\/[\d]+/.test(url.pathname)
+}
+
+export const isTikTokURL = (url: URL): boolean => {
+  if (url.hostname !== 'tiktok.com' && url.hostname !== 'www.tiktok.com') {
+    return false
+  }
+  return /\/[^/]+\/video\/[\d]+/.test(url.pathname)
+}
+
+export const isInstagramURL = (url: URL): boolean => {
+  if (
+    url.hostname !== 'instagram.com' &&
+    url.hostname !== 'www.instagram.com'
+  ) {
+    return false
+  }
+  return /\/p\/[^/]+/.test(url.pathname)
+}
+
+export const isPinterestURL = (url: URL): boolean => {
+  if (
+    url.hostname !== 'pinterest.com' &&
+    url.hostname !== 'www.pinterest.com' &&
+    url.hostname !== 'pinterest.jp' &&
+    url.hostname !== 'www.pinterest.jp'
+  ) {
+    return false
+  }
+  return /\/pin\/[\d]+/.test(url.pathname)
+}
+
+export const isCodePenURL = (url: URL): boolean => {
+  if (url.hostname !== 'codepen.io' && url.hostname !== 'www.codepen.io') {
+    return false
+  }
+  return /\/[^/]+\/pen\/[^/]+/.test(url.pathname)
+}
+
+export const isShortAmazonURL = (url: URL): boolean => {
+  if (url.hostname === 'amzn.to' || url.hostname === 'www.amzn.to') {
+    return true
+  }
+  return false
+}
+
+export const isFullAmazonURL = (url: URL): boolean => {
+  if (
+    url.hostname === 'amazon.com' ||
+    url.hostname === 'www.amazon.com' ||
+    url.hostname === 'amazon.co.jp' ||
+    url.hostname === 'www.amazon.co.jp'
+  ) {
+    return true
+  }
+  return false
+}
+
+export const isAmazonURL = (url: URL): boolean => {
+  return isShortAmazonURL(url) || isFullAmazonURL(url)
 }
 
 export const isYouTubeURL = (url: URL): boolean => {
